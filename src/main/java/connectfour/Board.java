89e7ec0a6b584d8a9755a8c4fb468979800576af
wtemuri <wtemuri@uoguelph.ca>
2022-11-13 00:36:16 +0000

    Started Class: Board and Connect4

--- src/main/java/connectfour/Board.java
+++ src/main/java/connectfour/Board.java
@@ -2,11 +2,101 @@ package connectfour;
 
 public class Board{
 
-/* this is a do-nothing method that was put here only so 
-you could have an example of junit testing.  Once you have other
-methods in the Board class and other tests you should delete
-this method and this comment */
-    public int returnSomething(){
+    private static int col = 7;
+    private static int row = 6;
+    private int[][] board = new int[row][col];
+
+    public void getBoard() {
+
+        System.out.println("|---|---|---|---|---|---|---|");
+        System.out.println(getRow(0));
+        System.out.println("|---|---|---|---|---|---|---|");
+        System.out.println(getRow(1));
+        System.out.println("|---|---|---|---|---|---|---|");
+        System.out.println(getRow(2));
+        System.out.println("|---|---|---|---|---|---|---|");
+        System.out.println(getRow(3));
+        System.out.println("|---|---|---|---|---|---|---|");
+        System.out.println(getRow(4));
+        System.out.println("|---|---|---|---|---|---|---|");
+        System.out.println(getRow(5));
+        System.out.println("|---|---|---|---|---|---|---|");
+        System.out.println(getRow(6));
+        System.out.println("|---|---|---|---|---|---|---|");
+    }
+
+    private String getRow(int r) {
+        //Instantiating a StringBuilder object to create modifiable string
+        StringBuilder newRow = new StringBuilder("| ");
+
+        //Looping through every row and appending appropriate values contingent on the board matrix values
+        for(int i = 0; i < col; i++) {
+            if (board[r][i] == 0) {
+                newRow.append("-");
+            }
+            if (board[r][i] == 1) {
+                newRow.append("O");
+            }
+            if (board[r][i] == 2) {
+                newRow.append("X");
+            }
+            newRow.append(" | ");
+        }
+
+        newRow.deleteCharAt(newRow.lastIndexOf(" "));
+        return newRow.toString();
+    }
+
+    private boolean isColumnFull(int column) {
+        return(board[0][column - 1] == 1 ||board[0][column - 1] == 2);
+    }
+
+    private boolean validInput (int input) {
+        return
+        ((input == 1) || 
+        (input == 2) || 
+        (input == 3) ||
+        (input == 4) || 
+        (input == 5) || 
+        (input == 6) || 
+        (input == 7));
+    }
+    
+    /**
+     * Gets the next available row for a given column slot
+     * @param column
+     * @return
+     */
+    private int getNextSlot(int column) { 
+        int pos = 5;
+        boolean found = false;
+
+        while(pos >= 0 && !found) {
+            if(!(board[pos][column] == 1) && !(board[pos][column] == 2)){
+                found = true;
+            }else {
+                pos--;
+            }
+        }
+        return pos;
+    }
+
+    public int placePiece(Player player) {
+
+        int colChoice = player.getMove() - 1;
+        String symbol = player.getPiece();
+
+        if (isColumnFull(colChoice)){
+            return 0;
+        }
+
+        if (symbol.equals("X"))
+        {
+            board[getNextSlot(colChoice)][colChoice] = 1;
+        } else {
+            board[getNextSlot(colChoice)][colChoice] = 2;
+        }
+
         return 1;
     }
 
---
+++ src/main/java/connectfour/Player.java
@@ -0,0 +1,64 @@
+package connectfour;
+
+/**
+ * This class is a Template for the Player Object
+ * <p>
+ * This class handles mutating and accessing player piece "X" or "O"
+ * </p>
+ * <p>
+ * This class also updates the move the user is currently wanting to play
+ * </p>
+ * @author Wali Temuri - 1183379
+ */
+public class Player {
+    
+    private String piece;
+    private int move;
+     
+    /**
+     * Sets the piece of a Player object
+     * 
+     * Returns void
+     * 
+     * @param String containing the selected piece
+     * @return void
+     */
+    public  void setPiece(String playerpiece) {
+
+        piece = playerpiece;
+    }
+     /**
+     * Sets the currentMove attribute of Player object
+     * 
+     * Returns void
+     * 
+     * @param int containing the move Player intends to make
+     * @return void
+     */
+    public void setMove(int m) {
+
+        m = move;
+    }
+     /**
+     * Get the piece of the Player 
+     * 
+     * Returns String 
+     * 
+     * @param void
+     * @return String containing the piece of Player 
+     */
+    public String getPiece() {
+        return piece;
+    }
+    /**
+     * Prints out the instructions for the Game
+     * 
+     * Returns int
+     * 
+     * @param void
+     * @return integer containing the move Player intends to make
+     */
+    public int getMove() {
+        return move;
+    }
+}
