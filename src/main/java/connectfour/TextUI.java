14e09339dfdf79dad5ff0f1caf1428de4c028031
wtemuri <wtemuri@uoguelph.ca>
2022-11-14 22:08:02 +0000

    Added Exception Handling for User

--- src/main/java/connectfour/TextUI.java
+++ src/main/java/connectfour/TextUI.java
@@ -3,14 +3,50 @@ import java.util.Scanner;
 
 public class TextUI{
 
+    private int getColInput(ConnectFour cfour) {
+        
+        boolean valid = false;
+        String strInput = "";
+        int num;
+
+        Scanner input = new Scanner(System.in);
+
+            while (!valid) {
+                System.out.println("Enter a column to place piece: ");
+               
+                strInput = input.next();
+    
+
+                try {
+                    num = Integer.parseInt(strInput);
+                    if (num > 7 || num < 1) {
+                        System.out.println("Error - enter an integer value between 1-7");
+                        valid = false;
+                    }else {
+                        if (cfour.isColumnFull(num)) {
+                            System.out.println("Error - Selected Column is Full.");
+                            valid = false;
+                        } else {
+                            valid = true;
+                            return num;
+                        }
+                    }
+                }catch (NumberFormatException e) {
+                    System.out.println("Error - enter an integer value between 1-7");
+                }
+            }
+        return 0;
+    }
+
+
     /**
      * @param cfour
      */
-    private static int getColInput() {
+  /*  private static int getColInput() {
 
         Scanner input = new Scanner(System.in);
         return input.nextInt();
-    }
+    }*/
 
     public void printWinner(int n) {
         System.out.println("Helo");
@@ -42,8 +78,8 @@ public class TextUI{
                 System.out.println("Its a draw");
             }
 
-            move = getColInput();
-            
+            move = userIO.getColInput(cFourGame);
+
             System.out.println(move);
             System.out.println(cFourGame.getTurn());
             if (cFourGame.getTurn() == 1) {
